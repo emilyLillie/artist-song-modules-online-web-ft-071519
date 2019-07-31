fb1fbfe8f110839406a41336d9f49cfcf425b42f
Emily Lillie <emilylillie3@gmail.com>
2019-07-31 18:26:01 +0000

    Automatically backed up by Learn

--- lib/concerns.rb/memorable.rb
+++ lib/concerns.rb/memorable.rb
@@ -11,3 +11,21 @@
 
   
 # end 
+
+module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+ 
+    def count
+      self.all.count
+    end
+  end
+ 
+  module InstanceMethods
+    def initialize
+      # some more code coming soon!
+    end
+  end
+end
